bbd26ec980965980cb1420155348583a25c022cd
DecoyGameDev <85084014+DecoyGameDev@users.noreply.github.com>
2022-03-23 21:47:33 -0400

    App modified

---
+++ SampleApp/app.js
@@ -0,0 +1,11 @@
+var quill = new Quill('#editor-container', {
+    modules: {
+      toolbar: [
+        [{ header: [1, 2, false] }],
+        ['bold', 'italic', 'underline'],
+        ['image', 'code-block']
+      ]
+    },
+    placeholder: 'Type here...',
+    theme: 'bubble'  // or 'bubble'
+  });
