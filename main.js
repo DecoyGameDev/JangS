1720541bea54dd4f0806d201dc88985c477b3cec
DecoyGameDev <85084014+DecoyGameDev@users.noreply.github.com>
2022-03-24 23:14:11 -0400

    Added create constructor

--- main.js
+++ main.js
@@ -1,8 +1,17 @@
 // UPDATE: ADD THIS.
+var currentfile = require(fileselected + '.txt');
+var fileselected = "orange";
 
+var input = "null";
+var constructor = ".null";
 
 function jangsbase() {
-    
+    if (input == "create"){
+        console.log("Create is not a valid command, add a constructor.")
+        if (constructor == ".file"){
+            
+        }
+    }
 }
 
 function jangsmodule2() {
